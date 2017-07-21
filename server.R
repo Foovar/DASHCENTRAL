71295d5b15a79f706fbe4ec7627c73f9a37b541e
Alex <aparedese@upao.edu.pe>
2017-07-20 20:44:00 -0500

    Primer reporte terminado

--- server.R
+++ server.R
@@ -4,20 +4,30 @@
 #
 # http://shiny.rstudio.com
 #
+setwd("/Users/Alex/Git/DASHCENTRAL")
 
 library(shiny)
+library(dplyr)
+source("scripts/reports/Report01.R")
+
+log <- read.csv("input/Log.csv")
+colnames(log) <- list( "src", "dst", "dcontext", "clid", "channel", "dstchannel", "lastapp", "lastdata", "start", "answer", "end", "duration", "billsec", "disposition", "amaflags", "uniqueid", "ip", "port" )
 
 shinyServer(function(input, output) {
 
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-  })
-
+ output$table <- DT::renderDataTable({
+   DT::datatable(log[ ,c(1,2,13,14, 9, 11)])
+ })
+ 
+ output$tcalls <- renderValueBox(
+   {
+     valueBox(format(length(log$src), digits = 3,  big.mark = ","), "Total de Llamadas", icon = icon("phone"), color="yellow")
+   }
+ )
+ 
+ output$report01 <- renderDygraph({
+   daily_report(log)
+ })
+ 
+ 
 })
--- ui.R
+++ ui.R
@@ -7,17 +7,17 @@
 # Central telefonica - Mainframe UPAO
 # 
 library(shiny)
-
 library(shinydashboard)
+library(dygraphs)
 
 dashboardPage(
   dashboardHeader(title = "CENTRAL REPORTS"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Data", tabName = "data", icon=icon("database")),
+      menuItem("Data", tabName = "data", icon=icon("database"), badgeLabel = "procesada"),
       menuItem("Charts", tabName = "charts", icon = icon("bar-chart"), 
-               menuSubItem("Llamadas a todo destino", icon = icon("phone")),
+               menuSubItem("Llamadas a todo destino", tabName = "report01", icon = icon("phone")),
                menuSubItem("Llamadas a soporte", icon = icon("support")),
                menuSubItem("Duracion de llamadas", icon = icon("clock-o")),
                menuSubItem("Llamadas sin exito", icon = icon("exclamation")),
@@ -26,5 +26,22 @@ dashboardPage(
                )
     )
   ),
-  dashboardBody()
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "dashboard", fluidRow(
+        valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
+        valueBox(10 * 2, "New Orders", icon = icon("credit-card"), color = "purple"),
+        valueBoxOutput("tcalls"),
+        box(
+          title = "Controls",
+          sliderInput("slider", "Number of observations:", 1, 100, 50)
+        )
+      )),
+      
+      tabItem(tabName = "data", fluidRow(
+          DT::dataTableOutput("table"))),
+      
+      tabItem(tabName = "report01", dygraphOutput("report01"))
+    )
+  )
 )
